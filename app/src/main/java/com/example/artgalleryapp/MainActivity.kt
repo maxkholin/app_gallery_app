5b574dfa191b9fb9b5df229cc20beb3738ae8f77
madmax <maksimkholin@gmail.com>
2024-09-25 15:27:35 +0300

    ArtworkWall done

--- app/src/main/java/com/example/artgalleryapp/MainActivity.kt
+++ app/src/main/java/com/example/artgalleryapp/MainActivity.kt
@@ -4,14 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
@@ -21,6 +26,7 @@ import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.artgalleryapp.data.DataProvider
 import com.example.artgalleryapp.ui.theme.ArtGalleryAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -38,13 +44,18 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun ArtGalleryApp() {
+    val artObjectsList = DataProvider.createListOfArtObjects()
+    val numberOfPictures = artObjectsList.size
+
+    val artObject = artObjectsList[0]
+
     Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(12.dp)
     ) {
         AppName()
-//        ArtworkWall()
+        ArtworkWall(artObject.imageId)
 //        ArtworkDescription()
 //        DisplayController()
     }
@@ -66,4 +77,19 @@ fun AppName() {
             .height(96.dp)
             .padding(top = 32.dp)
     )
+}
+
+@Composable
+fun ArtworkWall(imageId: Int) {
+    Surface(shadowElevation = 16.dp) {
+        Image(
+            painter = painterResource(imageId),
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(fraction = 0.6f)
+                .padding(24.dp),
+            contentScale = ContentScale.Crop
+        )
+    }
 }
