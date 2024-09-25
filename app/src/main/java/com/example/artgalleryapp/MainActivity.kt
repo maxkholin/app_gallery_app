bda1faef09de963b7c802b14663e1c207208c80a
madmax <maksimkholin@gmail.com>
2024-09-25 15:23:58 +0300

    app title done

--- app/src/main/java/com/example/artgalleryapp/MainActivity.kt
+++ app/src/main/java/com/example/artgalleryapp/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.artspaceapp
+package com.example.artgalleryapp
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -6,11 +6,21 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.artgalleryapp.ui.theme.ArtGalleryAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,8 +44,26 @@ fun ArtGalleryApp() {
             .padding(12.dp)
     ) {
         AppName()
-        ArtworkWall()
-        ArtworkDescription()
-        DisplayController()
+//        ArtworkWall()
+//        ArtworkDescription()
+//        DisplayController()
     }
+}
+
+@Composable
+fun AppName() {
+    Text(
+        text = stringResource(R.string.app_name),
+        fontSize = 24.sp,
+        textAlign = TextAlign.Center,
+        style = TextStyle(
+            fontWeight = FontWeight.Bold,
+            fontStyle = FontStyle.Normal,
+            textDecoration = TextDecoration.LineThrough
+        ),
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(96.dp)
+            .padding(top = 32.dp)
+    )
 }
